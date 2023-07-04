bee39c93979d4444240b8c716024e3eda533d16e
bushuev-alex <bushuev-alex@mail.ru>
2023-07-04 21:14:45 +0700

    1. add serializers to serializers.py

---
+++ fstr_api/api/serializers.py
@@ -0,0 +1,39 @@
+from api.models import *
+from rest_framework import serializers
+
+
+class UsersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = ['email', 'name', ]
+
+
+class CoordsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coords
+        fields = ['latitude', 'longitude', 'height', ]
+
+
+class PerevalAddedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PerevalAdded
+        fields = ['user_id', 'beautyTitle', 'title', 'other_titles', 'level', 'connect', 'date_added', 'add_first_time',
+                  'coord_id', 'status']
+
+
+class ImagesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Images
+        fields = ['date_added', 'title', 'img', ]
+
+
+class PerevalImagesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PerevalImages
+        fields = ['pereval_id', 'image_id', ]
+
+
+class SprActivitiesTypesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SprActivitiesTypes
+        fields = ['title', ]
