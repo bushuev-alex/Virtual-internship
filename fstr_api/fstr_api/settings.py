6b8009c13cdd24711b1c2056598e0985568f58e9
bushuev-alex <bushuev-alex@mail.ru>
2023-07-07 19:42:26 +0700

    1. correct mistakes in openapi-schema.yml

--- fstr_api/fstr_api/settings.py
+++ fstr_api/fstr_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from .keys import SECRET_KEY
 import os
 
 
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = SECRET_KEY  # os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
